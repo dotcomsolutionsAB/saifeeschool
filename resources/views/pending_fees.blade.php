87def9b0fe30cb99f41dffed22b8357a800d8146
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-01-04 16:35:00 +0530

    update for pdf

--- resources/views/pending_fees.blade.php
+++ resources/views/pending_fees.blade.php
@@ -10,6 +10,10 @@
         h3 {
             text-align: center;
             margin-bottom: 10px;
+            page-break-before: always;  
+        }
+        h3:first-of-type {
+            page-break-before: auto;
         }
         table {
             width: 100%;
@@ -31,28 +35,40 @@
         .fee-row {
             background-color: #fff;
         }
+        .total-row {
+            font-weight: bold;
+            background-color: #e0e0e0;
+        }
+        
+        .class-group {
+            page-break-before: always; /* Ensure each class starts on a new page */
+        }
+        .class-group:first-of-type {
+            page-break-before: auto; /* Prevent page break before the first class */
+        }
     </style>
 </head>
 <body>
     @foreach ($classes as $class)
+    <div class="class-group">
         <h3>{{ $class['class_name'] }} (Generated on: {{ now()->format('d-m-Y h:i:s A') }})</h3>
         <table>
             <thead>
                 <tr>
-                    <th>SN</th>
-                    <th>Student</th>
-                    <th>Roll No</th>
-                    <th>Fee</th>
-                    <th>Total</th>
+                    <th style="width: 5%;">SN</th>
+                    <th style="width: 35%;">Student</th>
+                    <th style="width: 15%;">Roll No</th>
+                    <th style="width: 40%;">Fee</th>
+                    <th style="width: 15%;">Total</th>
                 </tr>
             </thead>
             <tbody>
                 @php $sn = 1; @endphp
                 @foreach ($class['students'] as $student)
                     <tr class="student-row">
-                        <td rowspan="{{ count($student['fees']) }}">{{ $sn++ }}</td>
-                        <td rowspan="{{ count($student['fees']) }}">{{ $student['name'] }}</td>
-                        <td rowspan="{{ count($student['fees']) }}">{{ $student['roll_no'] }}</td>
+                        <td rowspan="{{ count($student['fees']) + 1 }}">{{ $sn++ }}</td>
+                        <td rowspan="{{ count($student['fees']) + 1 }}">{{ $student['name'] }}</td>
+                        <td rowspan="{{ count($student['fees']) + 1 }}">{{ $student['roll_no'] }}</td>
                         <td>{{ $student['fees'][0]['fee_name'] }}</td>
                         <td>{{ $student['fees'][0]['total'] }}</td>
                     </tr>
@@ -64,9 +80,15 @@
                         </tr>
                         @endif
                     @endforeach
+                    <!-- Total Row Inside the Table -->
+                    <tr class="total-row">
+                        <td>Total:</td>
+                        <td>{{ $student['student_total'] }}</td>
+                    </tr>
                 @endforeach
             </tbody>
         </table>
+    </div>
     @endforeach
 </body>
 </html>
