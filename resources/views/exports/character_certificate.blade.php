be1015d02272fb975ea0057ff2c0cd7d283eabe1
Husain990 <husain990@gmail.com>
2025-02-26 10:28:20 +0530

    sa

--- resources/views/exports/character_certificate.blade.php
+++ resources/views/exports/character_certificate.blade.php
@@ -4,21 +4,37 @@
     <meta charset="UTF-8">
     <title>Character Certificate</title>
     <style>
-        @page { size: A4; margin: 0;   transform: scale(1.2); /* Scale the entire document */}
-        body { font-family: Arial, sans-serif; margin: 0; padding: 0; font-size: 18px; /* 🔹 Increase font size */ background-image: url('{{ storage_path("app/public/pdf/cc.jpg") }}'); background-size: contain; width: 210mm; height: 297mm; position: relative; }
+        @page { size: A4; margin: 0; }
+        body { 
+            font-family: Arial, sans-serif; 
+            margin: 0; 
+            padding: 0; 
+            font-size: 22px; /* 🔹 Slightly increased base font size */
+            background-image: url('{{ storage_path("app/public/pdf/cc.jpg") }}'); 
+            background-size: cover; /* Ensures full background fit */
+            background-position: center; 
+            width: 210mm; 
+            height: 297mm; 
+            position: relative; 
+        }
         .container { position: absolute; width: 100%; height: 100%; padding: 20mm; }
-        .field { position: absolute; font-size: 46px; font-weight: bold; text-align: center; }
+        .field { 
+            position: absolute; 
+            font-size: 56px; /* 🔹 Increased font size for 300 DPI */
+            font-weight: bold; 
+            text-align: center; 
+        }
 
         /* Positioning Fields */
-        .serial-no { top: 82mm; left: 30mm; width: 20mm; }
-        .reg-no { top: 82mm; left: 172mm; width: 20mm; }
-        .name { top: 100mm; left: 50mm; width: 140mm; font-style: italic; }
-        .years { top: 109mm; left: 170mm; width: 30mm; font-style: italic; text-transform: uppercase; }
-        .joining-date { top: 116mm; left: 35mm; width: 30mm; font-style: italic; }
-        .leaving-date { top: 116mm; left: 85mm; width: 30mm; font-style: italic; }
-        .stream { top: 125mm; left: 50mm; width: 30mm; font-style: italic; }
-        .date-from { top: 125mm; left: 110mm; width: 50mm; font-style: italic; }
-        .dob { top: 145mm; left: 10mm; width: 190mm; font-style: italic; }
+        .serial-no { top: 82mm; left: 30mm; width: 25mm; }
+        .reg-no { top: 82mm; left: 172mm; width: 25mm; }
+        .name { top: 100mm; left: 50mm; width: 150mm; font-style: italic; }
+        .years { top: 109mm; left: 170mm; width: 40mm; font-style: italic; text-transform: uppercase; }
+        .joining-date { top: 116mm; left: 35mm; width: 35mm; font-style: italic; }
+        .leaving-date { top: 116mm; left: 85mm; width: 35mm; font-style: italic; }
+        .stream { top: 125mm; left: 50mm; width: 35mm; font-style: italic; }
+        .date-from { top: 125mm; left: 110mm; width: 60mm; font-style: italic; }
+        .dob { top: 145mm; left: 10mm; width: 200mm; font-style: italic; }
 
     </style>
 </head>
