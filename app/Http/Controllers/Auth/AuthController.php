aa01438023dc9ddd5a64f715af9663f79e5a6af3
Husain990 <husain990@gmail.com>
2025-03-12 21:23:30 +0530

    go

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\AcademicYearModel;
+use Illuminate\Support\Facades\DB; 
 
 class AuthController extends Controller
 {
