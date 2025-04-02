fc986f8afd26d5a9046a09a8ab9802fc2471bc85
Husain990 <husain990@gmail.com>
2025-04-02 21:50:17 +0530

    now

--- resources/views/exports/tabulation_pdf.blade.php
+++ resources/views/exports/tabulation_pdf.blade.php
@@ -1,48 +1,70 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <title>Tabulation Report</title>
     <style>
+        body {
+            font-family: Arial, sans-serif;
+            font-size: 12px;
+        }
         table {
             width: 100%;
             border-collapse: collapse;
-            font-size: 10px;
+            table-layout: fixed;
         }
         th, td {
-            border: 1px solid #333;
+            border: 1px solid #000;
             padding: 4px;
             text-align: center;
+            word-wrap: break-word;
         }
-        th.rotate {
-            height: 140px;
+        .rotate {
+            transform: rotate(-90deg);
             white-space: nowrap;
+            font-weight: bold;
         }
-        th.rotate > div {
-            transform: translate(0px, 60px) rotate(-90deg);
-            width: 20px;
+        .rotate-header {
+            height: 200px;
+            vertical-align: bottom;
+        }
+        .vertical-text {
+            writing-mode: vertical-lr;
+            transform: rotate(180deg);
         }
     </style>
 </head>
 <body>
-    <h2>Tabulation Report - {{ $data['class'] }} ({{ $data['year'] }})</h2>
-
+    <h2>Tabulation Report - {{ $data['class'] ?? 'Class' }} ({{ $data['year'] ?? 'Year' }})</h2>
     <table>
         <thead>
             <tr>
-                <th>Subject</th>
-                @foreach ($data['students'] as $student)
-                    <th>{{ $student->st_roll_no }}<br>{{ $student->st_first_name }} {{ $student->st_last_name }}</th>
+                <th rowspan="2">SN</th>
+                <th rowspan="2">Roll No</th>
+                <th rowspan="2">Name</th>
+                @foreach ($data['subjects'] as $subject)
+                    <th class="rotate-header">
+                        <div class="vertical-text">
+                            {{ $subject['subject_name'] ?? '' }}<br>
+                            <small>({{ $subject['category'] ?? '' }})</small>
+                        </div>
+                    </th>
                 @endforeach
             </tr>
         </thead>
         <tbody>
-            @foreach ($data['subjects'] as $subject)
+            @foreach ($data['students'] as $index => $student)
                 <tr>
-                    <td>{{ $subject['subject_name'] }} ({{ $subject['category'] }})</td>
-                    @foreach ($data['students'] as $student)
+                    <td>{{ $index + 1 }}</td>
+                    <td>{{ $student['st_roll_no'] ?? '' }}</td>
+                    <td>{{ $student['st_first_name'] ?? '' }} {{ $student['st_last_name'] ?? '' }}</td>
+                    @foreach ($data['subjects'] as $subject)
                         @php
-                            $val = $data['marks'][$student->st_id][$subject['subject_id']][$subject['category'] === 'Practical' ? 'prac' : 'marks'] ?? '';
+                            $subjId = $subject['subject_id'];
+                            $cat = $subject['category'] === 'Practical' ? 'prac' : 'marks';
+                            $mark = $data['marks'][$student['st_id']][$subjId][$cat] ?? '';
                         @endphp
-                        <td>{{ $val }}</td>
+                        <td>{{ $mark }}</td>
                     @endforeach
                 </tr>
             @endforeach
